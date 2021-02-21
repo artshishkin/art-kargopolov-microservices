67177f31208db8b7a5c652e8a790e45e5ad6be79
Art <d.art.shishkin@gmail.com>
2021-02-22 00:03:23 +0200

    42. Users Microservice - Create new Spring Boot Project

---
+++ photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.photoapp.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+    }
+
+}
---
+++ photo-app-api-users/src/test/java/net/shyshkin/study/photoapp/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
