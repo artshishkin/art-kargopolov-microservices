3121d533ae921173a0004836258ae514bc4ed333
Art <d.art.shishkin@gmail.com>
2021-02-21 12:10:54 +0200

    20. Reading Query String Request Parameters (#1)

--- mobile-app-ws/src/main/java/net/shyshkin/study/app/ws/ui/controllers/UserController.java
+++ mobile-app-ws/src/main/java/net/shyshkin/study/app/ws/ui/controllers/UserController.java
@@ -2,13 +2,16 @@ package net.shyshkin.study.app.ws.ui.controllers;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping(UserController.BASE_URL)
 public class UserController {
 
     public static final String BASE_URL = "/users";
+
+    @GetMapping
+    public String getAllUser(int page, int limit) {
+        return String.format("Get Users was called. Page %d of %d.", page, limit);
+    }
 
     @GetMapping("/{userId}")
     public String getUser(@PathVariable String userId) {
