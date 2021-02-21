87c8deb31866b0e225827b6d6db6076d3421cd05
Art <d.art.shishkin@gmail.com>
2021-02-21 16:19:15 +0200

    32. Handle a Specific Exception (#1)

--- mobile-app-ws/src/main/java/net/shyshkin/study/app/ws/exceptions/AppExceptionHandler.java
+++ mobile-app-ws/src/main/java/net/shyshkin/study/app/ws/exceptions/AppExceptionHandler.java
@@ -9,18 +9,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Arrays;
 import java.util.Date;
 
 @Slf4j
 @RestControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+//    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorMessage handleAnyException(Exception ex, WebRequest webRequest) {
 //        log.error("Handling Exception", ex);
         String message = ex.getLocalizedMessage();
         if (message == null) message = ex.toString();
+        return ErrorMessage.builder()
+                .message(message)
+                .timestamp(new Date())
+                .build();
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorMessage handleNPE(NullPointerException ex, WebRequest webRequest) {
+        String message = Arrays.toString(ex.getStackTrace());
         return ErrorMessage.builder()
                 .message(message)
                 .timestamp(new Date())
