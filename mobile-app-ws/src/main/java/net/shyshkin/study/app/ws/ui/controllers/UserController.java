a720d28824b7ebcfadc50cd98a557c8b6c83bacc
Art <d.art.shishkin@gmail.com>
2021-02-21 11:58:49 +0200

    19. Reading Path Variables with @PathVariable annotaion (#1)

--- mobile-app-ws/src/main/java/net/shyshkin/study/app/ws/ui/controllers/UserController.java
+++ mobile-app-ws/src/main/java/net/shyshkin/study/app/ws/ui/controllers/UserController.java
@@ -1,7 +1,8 @@
 package net.shyshkin.study.app.ws.ui.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(UserController.BASE_URL)
@@ -9,5 +10,23 @@ public class UserController {
 
     public static final String BASE_URL = "/users";
 
+    @GetMapping("/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "Get User was called for user " + userId;
+    }
 
+    @PostMapping
+    public String createUser(String user) {
+        return "Was created user: " + user;
+    }
+
+    @PutMapping
+    public String updateUser(String user) {
+        return "User was updated: " + user;
+    }
+
+    @DeleteMapping
+    public String deleteUser(String user) {
+        return "User was deleted: " + user;
+    }
 }
