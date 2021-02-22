518ce5fb1020b66ef8dead90f158c3654b7acd81
Art <d.art.shishkin@gmail.com>
2021-02-22 12:45:58 +0200

    44. Create Users Rest Controller

---
+++ photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/ui/controllers/UsersController.java
@@ -0,0 +1,15 @@
+package net.shyshkin.study.photoapp.api.users.ui.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @GetMapping("/status/check")
+    public String status(){
+        return "Working";
+    }
+}
