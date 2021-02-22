f2a20a84f702f42d0fa810844a7a18f6ef63115b
Art <d.art.shishkin@gmail.com>
2021-02-22 14:51:03 +0200

    60. Creating API Gateway Project

---
+++ photo-app-api-gateway/src/main/java/net/shyshkin/study/photoapp/api/gateway/PhotoAppApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.photoapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiGatewayApplication.class, args);
+    }
+
+}
