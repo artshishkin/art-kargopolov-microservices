0545c153fc48bedf2f14e9ffc282a759d9300100
Art <d.art.shishkin@gmail.com>
2021-02-23 22:23:57 +0200

    80. Generate Unique Public User Id (#2)

--- photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/services/UserServiceImpl.java
+++ photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/services/UserServiceImpl.java
@@ -3,11 +3,14 @@ package net.shyshkin.study.photoapp.api.users.services;
 import net.shyshkin.study.photoapp.api.users.shared.UserDto;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto createUser(UserDto userDetails) {
+        userDetails.setUserId(UUID.randomUUID());
         throw new RuntimeException("NOT IMPLEMENTED YET");
     }
 }
