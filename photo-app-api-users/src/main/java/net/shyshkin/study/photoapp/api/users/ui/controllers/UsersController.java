373cacf03c135fc17195ad93de6f4e54c7d0b246
Art <d.art.shishkin@gmail.com>
2021-02-23 16:27:27 +0200

    69. Trying How Load Balancer Works

--- photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/ui/controllers/UsersController.java
+++ photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,15 +1,20 @@
 package net.shyshkin.study.photoapp.api.users.ui.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UsersController {
 
+    private final Environment environment;
+
     @GetMapping("/status/check")
-    public String status(){
-        return "Working";
+    public String status() {
+        return environment.getProperty("spring.application.name") + " is running on port: " + environment.getProperty("local.server.port");
     }
 }
