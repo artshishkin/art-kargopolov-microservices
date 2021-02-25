e9ef2676d22ee18b0370df01ab52e121b756523d
Art <d.art.shishkin@gmail.com>
2021-02-25 14:38:43 +0200

    112. Reading Authorization HTTP Header (#5)

--- photo-app-api-gateway/src/main/java/net/shyshkin/study/photoapp/api/gateway/AuthorizationHeaderFilter.java
+++ photo-app-api-gateway/src/main/java/net/shyshkin/study/photoapp/api/gateway/AuthorizationHeaderFilter.java
@@ -1,11 +1,28 @@
 package net.shyshkin.study.photoapp.api.gateway;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Component
-public class AuthorizationHeaderFilter  extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    @Value("${token.secret}")
+    private String tokenSecret;
 
     public static class Config {
         // Put configuration properties here
@@ -13,6 +30,27 @@ public class AuthorizationHeaderFilter  extends AbstractGatewayFilterFactory<Aut
 
     @Override
     public GatewayFilter apply(Config config) {
-        return null;
+
+        GatewayFilter gatewayFilter = new GatewayFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                ServerHttpRequest request = exchange.getRequest();
+
+                if (!request.getHeaders().containsKey(AUTHORIZATION)) {
+                    return onError(exchange, "No authorization header", UNAUTHORIZED);
+                }
+                List<String> authorizationList = request.getHeaders().get(AUTHORIZATION);
+                String jwt = authorizationList.get(0).replace("Bearer ", "");
+
+                return chain.filter(exchange);
+            }
+        };
+        return gatewayFilter;
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
     }
 }
