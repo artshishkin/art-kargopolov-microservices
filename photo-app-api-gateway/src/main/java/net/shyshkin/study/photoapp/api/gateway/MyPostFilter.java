bac9085fc6a24d26e578cfef31f3f459d21027fb
Art <d.art.shishkin@gmail.com>
2021-02-26 12:22:30 +0200

    119. Creating Global Post Filter (#6)

---
+++ photo-app-api-gateway/src/main/java/net/shyshkin/study/photoapp/api/gateway/MyPostFilter.java
@@ -0,0 +1,24 @@
+package net.shyshkin.study.photoapp.api.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class MyPostFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        return chain.filter(exchange)
+
+                .then(Mono.fromRunnable(() -> {
+                    HttpHeaders headers = exchange.getResponse().getHeaders();
+                    log.debug("My first Post-Filter is executed with headers: {}", headers);
+                }));
+    }
+}
