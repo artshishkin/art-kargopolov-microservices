2c2b899068e884f8d232a182bb694649cd3a13e9
Art <d.art.shishkin@gmail.com>
2021-02-26 23:22:42 +0200

    125. Create Your Own Config Server (#7 Section 17: Spring Cloud Config Server - Git Backend)

---
+++ photo-app-api-config-server/src/main/java/net/shyshkin/study/photoapp/api/configserver/photoapp/api/configserver/PhotoAppApiConfigServerApplication.java
@@ -0,0 +1,15 @@
+package net.shyshkin.study.photoapp.api.configserver.photoapp.api.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class PhotoAppApiConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiConfigServerApplication.class, args);
+    }
+
+}
---
+++ photo-app-api-config-server/src/test/java/net/shyshkin/study/photoapp/api/configserver/photoapp/api/configserver/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.photoapp.api.configserver.photoapp.api.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
