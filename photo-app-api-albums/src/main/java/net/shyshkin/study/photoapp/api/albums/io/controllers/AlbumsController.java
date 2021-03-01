e7dca8c5ab74feeb74d0397e3c0a859dac4b95c3
Art <d.art.shishkin@gmail.com>
2021-03-01 14:45:02 +0200

    167. A walk through an Albums Microservice (#12)

--- photo-app-api-albums/src/main/java/net/shyshkin/study/photoapp/api/albums/io/controllers/AlbumsController.java
+++ photo-app-api-albums/src/main/java/net/shyshkin/study/photoapp/api/albums/io/controllers/AlbumsController.java
@@ -5,51 +5,46 @@
  */
 package net.shyshkin.study.photoapp.api.albums.io.controllers;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.photoapp.api.albums.data.AlbumEntity;
 import net.shyshkin.study.photoapp.api.albums.service.AlbumsService;
 import net.shyshkin.study.photoapp.api.albums.ui.model.AlbumResponseModel;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
 @RestController
 @RequestMapping("/users/{id}/albums")
+@Slf4j
+@RequiredArgsConstructor
 public class AlbumsController {
-    
-    @Autowired
-    AlbumsService albumsService;
-    Logger logger = LoggerFactory.getLogger(this.getClass());
-    @GetMapping( 
-            produces = { 
-                MediaType.APPLICATION_JSON_VALUE,
-                MediaType.APPLICATION_XML_VALUE,
-            })
-    public List<AlbumResponseModel> userAlbums(@PathVariable String id) {
 
-        List<AlbumResponseModel> returnValue = new ArrayList<>();
-        
+    private final AlbumsService albumsService;
+
+    private static final Type LIST_TYPE = new TypeToken<List<AlbumResponseModel>>() {
+    }.getType();
+
+    @GetMapping(produces = {APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE,})
+    public ResponseEntity<List<AlbumResponseModel>> userAlbums(@PathVariable String id) {
+
         List<AlbumEntity> albumsEntities = albumsService.getAlbums(id);
-        
-        if(albumsEntities == null || albumsEntities.isEmpty())
-        {
-            return returnValue;
-        }
-        
-        Type listType = new TypeToken<List<AlbumResponseModel>>(){}.getType();
- 
-        returnValue = new ModelMapper().map(albumsEntities, listType);
-        logger.info("Returning " + returnValue.size() + " albums");
-        return returnValue;
+
+        if (albumsEntities == null || albumsEntities.isEmpty())
+            return ResponseEntity.noContent().build();
+
+        List<AlbumResponseModel> returnValue = new ModelMapper().map(albumsEntities, LIST_TYPE);
+        log.debug("Returning {} albums", returnValue.size());
+        return ResponseEntity.ok(returnValue);
     }
 }
