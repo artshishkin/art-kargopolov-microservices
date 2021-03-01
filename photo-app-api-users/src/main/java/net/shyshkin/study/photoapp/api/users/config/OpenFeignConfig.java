866f2f32d5bec4bac3781a999ea6c592bb14f821
Art <d.art.shishkin@gmail.com>
2021-03-02 01:47:42 +0200

    176. Enable HTTP Requests Logging in Feign Client (#12)

--- photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/config/OpenFeignConfig.java
+++ photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/config/OpenFeignConfig.java
@@ -1,9 +1,16 @@
 package net.shyshkin.study.photoapp.api.users.config;
 
+import feign.Logger;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(basePackages = "net.shyshkin.study.photoapp.api.users.services")
 public class OpenFeignConfig {
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
