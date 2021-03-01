c6d30639c1bf4c104aae1ad8fbf1dcf326a9ea56
Art <d.art.shishkin@gmail.com>
2021-03-01 22:45:20 +0200

    172. Enable Feign in Spring Boot Project (#12)

---
+++ photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/config/OpenFeignConfig.java
@@ -0,0 +1,9 @@
+package net.shyshkin.study.photoapp.api.users.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class OpenFeignConfig {
+}
