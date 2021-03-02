62b7821f9074ddfa4ea62dffcc3f7ffb9b8da16d
Art <d.art.shishkin@gmail.com>
2021-03-02 19:04:51 +0200

    180. Configure Project to use Resilience4j Circuit Breaker (#12)

--- photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/services/AlbumsServiceClient.java
+++ photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/services/AlbumsServiceClient.java
@@ -2,16 +2,26 @@ package net.shyshkin.study.photoapp.api.users.services;
 
 import net.shyshkin.study.photoapp.api.users.ui.model.AlbumResponseModel;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "albums-ws")
+@FeignClient(name = "albums-ws", fallback = AlbumsServiceFallback.class)
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{userId}/albums404")
     List<AlbumResponseModel> getUserAlbums(@PathVariable UUID userId);
 
+}
+
+@Component
+class AlbumsServiceFallback implements AlbumsServiceClient {
+    @Override
+    public List<AlbumResponseModel> getUserAlbums(UUID userId) {
+        return Collections.emptyList();
+    }
 }
