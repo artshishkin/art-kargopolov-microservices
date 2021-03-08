856ab82407bd174411b3f73ce624937c81afa0d8
Art <d.art.shishkin@gmail.com>
2021-03-08 14:50:50 +0200

    214. Build Docker Image for Eureka Discovery Service (#18)

--- photo-app-discovery-service/src/test/java/net/shyshkin/study/photo/discovery/service/PhotoAppDiscoveryServiceApplicationTests.java
+++ photo-app-discovery-service/src/test/java/net/shyshkin/study/photo/discovery/service/PhotoAppDiscoveryServiceApplicationTests.java
@@ -2,8 +2,14 @@ package net.shyshkin.study.photo.discovery.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+        properties = {
+                "spring.cloud.config.enabled=false"
+        }
+)
 class PhotoAppDiscoveryServiceApplicationTests {
 
     @Test
