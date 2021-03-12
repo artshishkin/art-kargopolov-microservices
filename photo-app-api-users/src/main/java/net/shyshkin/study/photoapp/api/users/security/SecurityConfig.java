c4dfddda42cbb546a0a7b4a6d1747d1a8df73a18
Art <d.art.shishkin@gmail.com>
2021-03-12 18:04:59 +0200

    241. Enable Method-Level Security (#20)

--- photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/security/SecurityConfig.java
+++ photo-app-api-users/src/main/java/net/shyshkin/study/photoapp/api/users/security/SecurityConfig.java
@@ -9,14 +9,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.net.InetAddress;
 
 @Slf4j
-@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
